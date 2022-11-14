4ce3e6733bdb084f1631cc3953d15471e882daed
JosePinos <jose.pinos@epn.edu.ec>
2022-11-14 15:47:49 -0500

    Creación del objeto 1

--- Trabajo_en_equipo/src/Main.java
+++ Trabajo_en_equipo/src/Main.java
@@ -7,6 +7,9 @@ public class Main {
         boolean salir = false;
         int opcion;
 
+        //Objeto 1
+        Ciudadanos ciudadano_1 = new Ciudadanos();
+
         //bucle para comenzar el menu
         while(!salir){
             System.out.println("1.- Persona 1");
@@ -26,6 +29,20 @@ public class Main {
             opcion = sn.nextInt();
             switch (opcion){
                 case 1:
+
+                    ciudadano_1.setNombre("José Pinos");
+                    ciudadano_1.setEdad(21);
+                    ciudadano_1.setCedula("1726908138");
+                    ciudadano_1.setEstado_civil("Soltero");
+                    ciudadano_1.setNombre_github("JosePinos 1) J-Pinos3 2)");
+
+                    System.out.println("\tCiudadano 1");
+                    System.out.println("Nombre: " +ciudadano_1.getNombre() );
+                    System.out.println("Edad: " +ciudadano_1.getEdad() );
+                    System.out.println("C.I.: " + ciudadano_1.getCedula() );
+                    System.out.println("Estado Civil: " + ciudadano_1.getEstado_civil() );
+                    System.out.println("Github: " + ciudadano_1.getNombre_github() );
+
                     break;
                 case 2:
                     break;
